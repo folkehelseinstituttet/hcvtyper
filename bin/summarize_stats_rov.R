d1ce56f41239807e18692eb54e51219fed8aa2d9
Jon Bråte <jon.brate@fhi.no>
2024-08-21 09:40:29 +0000

    Updates

--- bin/summarize_stats_rov.R
+++ bin/summarize_stats_rov.R
@@ -10,25 +10,37 @@ if (length(args) < 2) {
 prefix <- args[1]
 stats  <- args[2]
 
-df <- as.data.frame(matrix(nrow = 1, ncol = 3))
-colnames(df) <- c("sampleName", "contig", "mapped_reads")
+df <- as.data.frame(matrix(nrow = 1, ncol = 4))
+colnames(df) <- c("sampleName", "gene", "contig", "mapped_reads")
 
 # Read the stats file, extract the name of the sequence that was mapped to and the number of mapped reads
-mapped_seq <- read_tsv(stats, col_names = FALSE, n_max = 7) %>%
+tmp <- read_tsv(stats, col_names = FALSE, n_max = 7) %>%
     # Extract the command used
     filter(str_detect(X1, "The command line was")) %>%
     # Separate out the pattern after the string "reference"
     separate(X1, into = c(NA, "tmp"), sep = "reference ") %>%
-    # Separate out the pattern before the string ".fasta"
-    separate(tmp, into = c("seq", NA), sep = "\\.fasta")
+    # Separate out info from the fasta file name
+    separate(tmp, into = c("sample", "gene,", "seq", NA), sep = "\\.")
 
-mapped_reads <- read_tsv(stats, col_names = FALSE, comment = "#") %>%
-    filter(str_detect(X1, "reads mapped:")) %>%
-    separate(X1, into = c(NA, "reads"), sep = "reads mapped:   ") %>%
-    pull(reads)
+mapped_seq <- tmp %>% pull(seq)
+prefix_2 <- tmp %>% pull(sample)
+gene <- tmp %>% pull(`gene,`)
 
-df$sampleName <- str_remove(prefix, "\\.withdup")
+# Compare the input prefix with prefix_2. Error if they are not identical
+if (str_remove(prefix, "\\.withdup") != prefix_2) {
+    stop("Prefixes do not match: ", prefix, " vs ", prefix_2, call.=FALSE)
+}
+
+mapped_reads <- read_table(stats, col_names = FALSE, comment = "#", n_max = 10) %>%
+    filter(X3 == "mapped:") %>%
+    #separate(X1, into = c(NA, "reads"), sep = "reads mapped:   ") %>%
+    pull(X4)
+
+df$sampleName <- prefix_2
 df$contig <- mapped_seq
+df$gene <- gene
 df$mapped_reads <- mapped_reads
 
-write_csv(paste0(prefix, ".", mapped_seq, ".csv"), df)
+df <- tibble(df)
+
+write_csv(df, paste0(prefix, ".", mapped_seq, ".csv"))
