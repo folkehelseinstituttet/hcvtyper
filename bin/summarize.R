4205cd3b9879c9a8a7d50f3d740c4b0e8d15ab8a
Jon Bråte <jon.brate@fhi.no>
2024-11-08 13:55:44 +0100

    Revert back to write_csv

--- bin/summarize.R
+++ bin/summarize.R
@@ -536,6 +536,6 @@ if ("^Major_minor$" %in% colnames(lw_import)) {
 }
 
 # Write file
-write_csv2(lw_import, file = "Genotype_mapping_summary_long_LW_import.csv")
+write_csv(lw_import, file = "Genotype_mapping_summary_long_LW_import.csv")
 
 
